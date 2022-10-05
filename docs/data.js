9018b8c7f33403c1d4a75170b6544c8146d8c986
github-action-benchmark <github@users.noreply.github.com>
2022-10-05 15:38:10 +0000

    add Go Benchmark (go) benchmark result for 809b5719f4f3a28e4a38735a330a7ec6f0e46a6d

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664984238827,
+  "lastUpdate": 1664984290264,
   "repoUrl": "https://github.com/ringsaturn/benchmark4fun",
   "entries": {
     "Go Benchmark": [
@@ -96,6 +96,54 @@ window.BENCHMARK_DATA = {
             "value": 50.02,
             "unit": "ns/op",
             "extra": "23002683 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ringsaturn.me@gmail.com",
+            "name": "ringsaturn",
+            "username": "ringsaturn"
+          },
+          "committer": {
+            "email": "ringsaturn.me@gmail.com",
+            "name": "ringsaturn",
+            "username": "ringsaturn"
+          },
+          "distinct": true,
+          "id": "809b5719f4f3a28e4a38735a330a7ec6f0e46a6d",
+          "message": "auto update deps",
+          "timestamp": "2022-10-05T23:37:31+08:00",
+          "tree_id": "91bd3b750988014f4b474d5762118fe278710919",
+          "url": "https://github.com/ringsaturn/benchmark4fun/commit/809b5719f4f3a28e4a38735a330a7ec6f0e46a6d"
+        },
+        "date": 1664984289308,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGenTile",
+            "value": 129.9,
+            "unit": "ns/op",
+            "extra": "9192537 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGenHex",
+            "value": 1386,
+            "unit": "ns/op",
+            "extra": "910310 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFormatFmt",
+            "value": 226.7,
+            "unit": "ns/op",
+            "extra": "5191562 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFormatNaive",
+            "value": 64.45,
+            "unit": "ns/op",
+            "extra": "18764704 times\n2 procs"
           }
         ]
       }
