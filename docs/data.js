448c4b481eadb04cb121343f3cefbc09f7062540
github-action-benchmark <github@users.noreply.github.com>
2022-10-06 01:31:40 +0000

    add Go Benchmark (go) benchmark result for f5d5f5451ed3d6393a366c0ff7138a49c5969c4a

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664984635136,
+  "lastUpdate": 1665019900037,
   "repoUrl": "https://github.com/ringsaturn/benchmark4fun",
   "entries": {
     "Go Benchmark": [
@@ -48,6 +48,52 @@ window.BENCHMARK_DATA = {
             "value": 68.71,
             "unit": "ns/op",
             "extra": "17922799 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ringsaturn",
+            "username": "ringsaturn",
+            "email": "ringsaturn.me@gmail.com"
+          },
+          "committer": {
+            "name": "ringsaturn",
+            "username": "ringsaturn",
+            "email": "ringsaturn.me@gmail.com"
+          },
+          "id": "f5d5f5451ed3d6393a366c0ff7138a49c5969c4a",
+          "message": "rename",
+          "timestamp": "2022-10-05T15:42:49Z",
+          "url": "https://github.com/ringsaturn/benchmark4fun/commit/f5d5f5451ed3d6393a366c0ff7138a49c5969c4a"
+        },
+        "date": 1665019899131,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkOrbMaptileGenTile",
+            "value": 117.1,
+            "unit": "ns/op",
+            "extra": "10767152 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkUberH3GenHex",
+            "value": 1348,
+            "unit": "ns/op",
+            "extra": "913210 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkUberH3FormatFmt",
+            "value": 248.1,
+            "unit": "ns/op",
+            "extra": "4934872 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkUberH3FormatNaive",
+            "value": 68.68,
+            "unit": "ns/op",
+            "extra": "16950378 times\n2 procs"
           }
         ]
       }
